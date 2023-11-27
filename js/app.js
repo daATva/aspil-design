1ca9c91e807196984e739b93565abb528b3a12dc
daATva <riven1231@mail.ru>
2023-11-27 22:44:04 +0400

    lern js

--- js/app.js
+++ js/app.js
@@ -12,3 +12,68 @@ function clearActiveClasses(){
         slide.classList.remove('active')
     })
 }
+
+// Создайте пустой объект user.
+// Добавьте свойство name со значением John.
+// Добавьте свойство surname со значением Smith.
+// Измените значение свойства name на Pete.
+// Удалите свойство name из объекта.
+
+
+  let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+
+
+    let sum = 0
+    for( let key in salaries){
+        sum += salaries[key]
+    }
+
+console.log(sum)
+    
+    // до вызова функции
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  }
+  
+
+
+function multiplyNumeric(obj){
+    for( let key in obj){
+        if (typeof obj[key] == 'number'){
+            obj[key] *= 2
+        }
+    }
+}
+
+multiplyNumeric(menu);
+
+console.log(menu)
+
+// Здесь функция makeUser возвращает объект.
+
+// Каким будет результат при обращении к свойству объекта ref? Почему?
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this
+  };
+}
+
+let user = makeUser();
+
+alert( user.ref.name ); // Каким будет результат?
+
+let calculator = {
+    // ... ваш код ...
+  };
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
